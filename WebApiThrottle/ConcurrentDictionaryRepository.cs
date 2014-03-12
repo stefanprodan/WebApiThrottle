b32ff8e55bad3a18af8183e464fc7e2c9a726da6
Stefan Prodan <stefan.prodan@gmail.com>
2014-03-13 01:16:15 +0200

    System runtime cache repo added for Owin self host use

---
+++ WebApiThrottle/ConcurrentDictionaryRepository.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiThrottle
+{
+    /// <summary>
+    /// Stors throttle metrics in a thread safe dictionary, has no clean-up mechanism, expired counters are deleted on renewal
+    /// </summary>
+    public class ConcurrentDictionaryRepository : IThrottleRepository
+    {
+        [Serializable]
+        internal struct ThrottleCounterWrapper
+        {
+            public DateTime Timestamp { get; set; }
+            public long TotalRequests { get; set; }
+            public TimeSpan ExpirationTime { get; set; }
+        }
+
+        private static ConcurrentDictionary<string, ThrottleCounterWrapper> cache = new ConcurrentDictionary<string, ThrottleCounterWrapper>();
+
+        public bool Any(string id)
+        {
+            return cache.ContainsKey(id);
+        }
+
+        /// <summary>
+        /// Insert or update
+        /// </summary>
+        public ThrottleCounter? FirstOrDefault(string id)
+        {
+            var entry = new ThrottleCounterWrapper();
+
+            if (cache.TryGetValue(id, out entry))
+            {
+                //remove expired entry
+                if (entry.Timestamp + entry.ExpirationTime < DateTime.UtcNow)
+                {
+                    cache.TryRemove(id, out entry);
+                    return null;
+                }
+            }
+
+            return new ThrottleCounter
+            {
+                Timestamp = entry.Timestamp,
+                TotalRequests = entry.TotalRequests
+            };
+        }
+
+        public void Save(string id, ThrottleCounter throttleCounter, TimeSpan expirationTime)
+        {
+            var entry = new ThrottleCounterWrapper
+            {
+                ExpirationTime = expirationTime,
+                Timestamp = throttleCounter.Timestamp,
+                TotalRequests = throttleCounter.TotalRequests
+            };
+
+            cache.AddOrUpdate(id, entry, (k, e) => entry);
+        }
+
+        public void Remove(string id)
+        {
+            var entry = new ThrottleCounterWrapper();
+            cache.TryRemove(id, out entry);
+        }
+
+        public void Clear()
+        {
+            cache.Clear();
+        }
+    }
+}
---
+++ WebApiThrottle/MemoryCacheRepository.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiThrottle
+{
+    /// <summary>
+    /// Stors throttle metrics in runtime cache, intented for owin self host.
+    /// </summary>
+    public class MemoryCacheRepository : IThrottleRepository
+    {
+        ObjectCache memCache = MemoryCache.Default;
+
+        /// <summary>
+        /// Insert or update
+        /// </summary>
+        public void Save(string id, ThrottleCounter throttleCounter, TimeSpan expirationTime)
+        {
+            if (memCache[id] != null)
+            {
+                memCache[id] = throttleCounter;
+            }
+            else
+            {
+                memCache.Add(
+                    id,
+                    throttleCounter, new CacheItemPolicy()
+                    {
+                        SlidingExpiration = expirationTime
+                    });
+            }
+        }
+
+        public bool Any(string id)
+        {
+            return memCache[id] != null;
+        }
+
+        public ThrottleCounter? FirstOrDefault(string id)
+        {
+            return (ThrottleCounter?)memCache[id];
+        }
+
+        public void Remove(string id)
+        {
+            memCache.Remove(id);
+        }
+
+        public void Clear()
+        {
+            var cacheKeys = memCache.Where(kvp => kvp.Value.GetType() == typeof(ThrottleCounter)).Select(kvp => kvp.Key).ToList();
+            foreach (string cacheKey in cacheKeys)
+            {
+                memCache.Remove(cacheKey);
+            }
+        }
+    }
+}
