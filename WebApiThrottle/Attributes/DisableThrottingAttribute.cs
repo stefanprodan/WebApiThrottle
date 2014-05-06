00eab29b2d25fdf2ead9beb7860891d45ad002b0
Stefan Prodan <stefan.prodan@gmail.com>
2014-05-06 16:58:24 +0300

    Added Enable/Disable attributes

---
+++ WebApiThrottle/Attributes/DisableThrottingAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.Filters;
+
+namespace WebApiThrottle
+{
+    public class DisableThrottingAttribute : ActionFilterAttribute, IActionFilter
+    {
+
+    }
+}
---
+++ WebApiThrottle/Attributes/EnableThrottlingAttribute.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.Filters;
+
+namespace WebApiThrottle
+{
+    public class EnableThrottlingAttribute : ActionFilterAttribute, IActionFilter
+    {
+        public long PerSecond { get; set; }
+        public long PerMinute { get; set; }
+        public long PerHour { get; set; }
+        public long PerDay { get; set; }
+        public long PerWeek { get; set; }
+
+        public long GetLimit(RateLimitPeriod period)
+        {
+            switch (period)
+            {
+                case RateLimitPeriod.Second:
+                    return PerSecond;
+                case RateLimitPeriod.Minute:
+                    return PerMinute;
+                case RateLimitPeriod.Hour:
+                    return PerHour;
+                case RateLimitPeriod.Day:
+                    return PerDay;
+                case RateLimitPeriod.Week:
+                    return PerWeek;
+                default:
+                    return PerSecond;
+            }
+        }
+    }
+}
