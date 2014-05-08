49f5f25d407448a3f47f41c57763e8870abfbd1d
Stefan Prodan <stefan.prodan@gmail.com>
2014-05-09 00:31:04 +0300

    Policy Repository added

---
+++ WebApiThrottle/Repositories/IPolicyRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace WebApiThrottle
+{
+    public interface IPolicyRepository
+    {
+        ThrottlePolicy FirstOrDefault(string id);
+        void Remove(string id);
+        void Save(string id, ThrottlePolicy policy);
+    }
+}
---
+++ WebApiThrottle/Repositories/PolicyCacheRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Caching;
+
+namespace WebApiThrottle
+{
+    public class PolicyCacheRepository : IPolicyRepository
+    {
+        public void Save(string id, ThrottlePolicy policy)
+        {
+            if (HttpContext.Current.Cache[id] != null)
+            {
+                HttpContext.Current.Cache[id] = policy;
+            }
+            else
+            {
+                HttpContext.Current.Cache.Add(
+                    id,
+                    policy,
+                    null,
+                    Cache.NoAbsoluteExpiration,
+                    Cache.NoSlidingExpiration,
+                    CacheItemPriority.High,
+                    null);
+            }
+        }
+
+        public ThrottlePolicy FirstOrDefault(string id)
+        {
+            var policy = (ThrottlePolicy)HttpContext.Current.Cache[id];
+            return policy;
+        }
+
+        public void Remove(string id)
+        {
+            HttpContext.Current.Cache.Remove(id);
+        }
+    }
+}
