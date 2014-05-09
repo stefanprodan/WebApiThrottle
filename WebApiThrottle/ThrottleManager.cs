164781c97d4eefeb3817f2ba042da48efbf81f9c
Stefan Prodan <stefan.prodan@gmail.com>
2014-05-09 19:23:07 +0300

    Added Throttle manager that allows changing the cache keys prefix

---
+++ WebApiThrottle/ThrottleManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiThrottle
+{
+    public class ThrottleManager
+    {
+        public static string ApplicationName = "";
+        public static string ThrottleKey = "throttle";
+        public static string PolicyKey = "throttle_policy";
+
+        public static string GetThrottleKey()
+        {
+            return ApplicationName + ThrottleKey;
+        }
+
+        public static string GetPolicyKey()
+        {
+            return ApplicationName + PolicyKey;
+        }
+    }
+}
