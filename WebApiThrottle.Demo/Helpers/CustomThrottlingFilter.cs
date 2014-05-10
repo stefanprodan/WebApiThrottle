45af62da3dde9c92665814c379c800db68e73c5e
Stefan Prodan <stefan.prodan@gmail.com>
2014-05-11 00:22:48 +0300

    Custom ThrottlingFilter example added

---
+++ WebApiThrottle.Demo/Helpers/CustomThrottlingFilter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+
+namespace WebApiThrottle.Demo.Helpers
+{
+    public class CustomThrottlingFilter : ThrottlingFilter
+    {
+        public CustomThrottlingFilter(ThrottlePolicy policy, IPolicyRepository policyRepository, IThrottleRepository repository, IThrottleLogger logger)
+            : base(policy, policyRepository, repository, logger)
+        {
+            this.QuotaExceededMessage = "API calls quota exceeded! maximum admitted {0} per {1}.";
+        }
+
+        protected override RequestIdentity SetIndentity(HttpRequestMessage request)
+        {
+            return new RequestIdentity()
+            {
+                ClientKey = request.Headers.Contains("Authorization-Key") ? request.Headers.GetValues("Authorization-Key").First() : "anon",
+                ClientIp = base.GetClientIp(request).ToString(),
+                Endpoint = request.RequestUri.AbsolutePath
+            };
+        }
+
+        protected override HttpResponseMessage QuotaExceededResponse(HttpRequestMessage request, string message, HttpStatusCode responseCode, string retryAfter)
+        {
+            var response = request.CreateResponse(responseCode, message);
+            response.Headers.Add("Retry-After", new string[] { retryAfter });
+            return response;
+        }
+    }
+}
