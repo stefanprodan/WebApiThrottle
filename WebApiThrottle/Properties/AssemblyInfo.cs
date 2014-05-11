9b4ed2d73c38d9152f3ba170950fd23df10f681c
Stefan Prodan <stefan.prodan@gmail.com>
2014-05-11 18:26:34 +0300

    release v1.2

--- WebApiThrottle/Properties/AssemblyInfo.cs
+++ WebApiThrottle/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("WebApiThrottle")]
-[assembly: AssemblyDescription("Throttling handler for ASP.NET Web API v2")]
+[assembly: AssemblyDescription("Throttling handler and filter for ASP.NET Web API v2")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("stefanprodan.eu")]
+[assembly: AssemblyCompany("stefanprodan.com")]
 [assembly: AssemblyProduct("WebApiThrottle")]
-[assembly: AssemblyCopyright("Copyright Stefan Prodan ©  2013")]
+[assembly: AssemblyCopyright("Copyright Stefan Prodan © 2013-2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
