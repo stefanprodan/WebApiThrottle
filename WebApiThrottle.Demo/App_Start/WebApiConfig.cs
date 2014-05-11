a5a3e8064908641cdc157d71f4a88645b8df66ca
Stefan Prodan <stefan.prodan@gmail.com>
2014-05-11 13:38:22 +0300

    Handler and Filter examples updated

--- WebApiThrottle.Demo/App_Start/WebApiConfig.cs
+++ WebApiThrottle.Demo/App_Start/WebApiConfig.cs
@@ -29,9 +29,8 @@ namespace WebApiThrottle.Demo
             config.EnableSystemDiagnosticsTracing();
 
             //Web API throttling handler
-            //config.MessageHandlers.Add(new ThrottlingHandler()
-            //{
-            //    Policy = new ThrottlePolicy(perSecond: 1, perMinute: 20, perHour: 30, perDay: 35, perWeek: 3000)
+            //config.MessageHandlers.Add(new ThrottlingHandler(
+            //    policy: new ThrottlePolicy(perMinute: 20, perHour: 30, perDay: 35, perWeek: 3000)
             //    {
             //        //scope to IPs
             //        IpThrottling = true,
@@ -53,64 +52,57 @@ namespace WebApiThrottle.Demo
             //        //white list API keys that don’t require throttling
             //        ClientWhitelist = new List<string> { "admin-key" },
 
-            //        //scope to routes (IP + Client Key + Request URL)
-            //        EndpointThrottling = true,
-            //        EndpointRules = new Dictionary<string, RateLimits>
-            //        { 
-            //            { "api/values/", new RateLimits { PerSecond = 3 } },
-            //            { "api/values", new RateLimits { PerSecond = 4 } }
-            //        }
+            //        //Endpoint rate limits will be loaded from EnableThrottling attribute
+            //        EndpointThrottling = true
             //    },
-            //    Repository = new CacheRepository(),
-            //    Logger = new TracingThrottleLogger(traceWriter)
-            //});
+            //    policyRepository: new PolicyCacheRepository(),
+            //    repository: new CacheRepository(),
+            //    logger: new TracingThrottleLogger(traceWriter)));
 
             //Web API throttling handler load policy from web.config
-            //config.MessageHandlers.Add(new ThrottlingHandler()
-            //{
-            //    Policy = ThrottlePolicy.FromStore(new PolicyConfigurationProvider()),
-            //    Repository = new CacheRepository(),
-            //    Logger = new TracingThrottleLogger(traceWriter)
-            //});
+            //config.MessageHandlers.Add(new ThrottlingHandler(
+            //    policy: ThrottlePolicy.FromStore(new PolicyConfigurationProvider()),
+            //    policyRepository: new PolicyCacheRepository(),
+            //    repository: new CacheRepository(),
+            //    logger: new TracingThrottleLogger(traceWriter)));
 
             //Web API throttling filter
-            config.Filters.Add(new ThrottlingFilter(
-                policy: new ThrottlePolicy(perMinute: 20, perHour: 30, perDay: 35, perWeek: 3000)
-                {
-                    //scope to IPs
-                    IpThrottling = true,
-                    IpRules = new Dictionary<string, RateLimits>
-                    { 
-                        { "::1/10", new RateLimits { PerSecond = 2 } },
-                        { "192.168.2.1", new RateLimits { PerMinute = 30, PerHour = 30*60, PerDay = 30*60*24 } }
-                    },
-                    //white list the "::1" IP to disable throttling on localhost for Win8
-                    IpWhitelist = new List<string> { "127.0.0.1", "192.168.0.0/24" },
+            //config.Filters.Add(new ThrottlingFilter(
+            //    policy: new ThrottlePolicy(perMinute: 20, perHour: 30, perDay: 35, perWeek: 3000)
+            //    {
+            //        //scope to IPs
+            //        IpThrottling = true,
+            //        IpRules = new Dictionary<string, RateLimits>
+            //        { 
+            //            { "::1/10", new RateLimits { PerSecond = 2 } },
+            //            { "192.168.2.1", new RateLimits { PerMinute = 30, PerHour = 30*60, PerDay = 30*60*24 } }
+            //        },
+            //        //white list the "::1" IP to disable throttling on localhost for Win8
+            //        IpWhitelist = new List<string> { "127.0.0.1", "192.168.0.0/24" },
 
-                    //scope to clients (if IP throttling is applied then the scope becomes a combination of IP and client key)
-                    ClientThrottling = true,
-                    ClientRules = new Dictionary<string, RateLimits>
-                    { 
-                        { "api-client-key-1", new RateLimits { PerMinute = 60, PerHour = 600 } },
-                        { "api-client-key-9", new RateLimits { PerDay = 5000 } }
-                    },
-                    //white list API keys that don’t require throttling
-                    ClientWhitelist = new List<string> { "admin-key" },
+            //        //scope to clients (if IP throttling is applied then the scope becomes a combination of IP and client key)
+            //        ClientThrottling = true,
+            //        ClientRules = new Dictionary<string, RateLimits>
+            //        { 
+            //            { "api-client-key-1", new RateLimits { PerMinute = 60, PerHour = 600 } },
+            //            { "api-client-key-9", new RateLimits { PerDay = 5000 } }
+            //        },
+            //        //white list API keys that don’t require throttling
+            //        ClientWhitelist = new List<string> { "admin-key" },
 
-                    //Endpoint rate limits will be loaded from EnableThrottling attribute
-                    EndpointThrottling = true
-                },
+            //        //Endpoint rate limits will be loaded from EnableThrottling attribute
+            //        EndpointThrottling = true
+            //    },
+            //    policyRepository: new PolicyCacheRepository(),
+            //    repository: new CacheRepository(),
+            //    logger: new TracingThrottleLogger(traceWriter)));
+
+            //Web API throttling filter load policy from web.config
+            config.Filters.Add(new ThrottlingFilter(           
+                policy: ThrottlePolicy.FromStore(new PolicyConfigurationProvider()),
                 policyRepository: new PolicyCacheRepository(),
                 repository: new CacheRepository(),
                 logger: new TracingThrottleLogger(traceWriter)));
-
-            //Web API throttling filter load policy from web.config
-            //config.Filters.Add(new ThrottlingFilter()
-            //{
-            //    Policy = ThrottlePolicy.FromStore(new PolicyConfigurationProvider()),
-            //    Repository = new CacheRepository(),
-            //    Logger = new TracingThrottleLogger(traceWriter)
-            //});
         }
     }
 }
