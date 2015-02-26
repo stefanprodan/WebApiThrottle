4acc6e451c56de72a6d646875a7ee1e6361e2032
Stefan Prodan <stefan.prodan@gmail.com>
2015-02-26 19:43:28 +0200

    release v1.4
    Remove Microsoft.AspNet.WebApi dependency, update WebApi.Core, Json and
    Owin deps to latest version, fix #23

--- WebApiThrottle/Properties/AssemblyInfo.cs
+++ WebApiThrottle/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("stefanprodan.com")]
 [assembly: AssemblyProduct("WebApiThrottle")]
-[assembly: AssemblyCopyright("Copyright Stefan Prodan © 2013-2014")]
+[assembly: AssemblyCopyright("Copyright Stefan Prodan © 2013-2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
