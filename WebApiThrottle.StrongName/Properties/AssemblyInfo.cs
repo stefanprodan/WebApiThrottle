1750561e8d95750adc31709d4c8b967c5f538afb
Stefan Prodan <stefan.prodan@gmail.com>
2016-11-12 16:10:30 +0200

    strong-named version #77 #39

--- WebApiThrottle.StrongName/Properties/AssemblyInfo.cs
+++ WebApiThrottle.StrongName/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("WebApiThrottle.StrongName")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Throttling handler and filter for ASP.NET Web API, strong-named version")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("stefanprodan.com")]
 [assembly: AssemblyProduct("WebApiThrottle.StrongName")]
 [assembly: AssemblyCopyright("Copyright © Stefan Prodan 2016")]
 [assembly: AssemblyTrademark("")]
