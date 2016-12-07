da73c9641e6497576e3f90c4655dab1b293a97f1
Jimmy Stridh <jimmy@stridh.nu>
2016-12-07 14:41:44 +0100

    added test project and failing test

---
+++ WebApiThrottle.Tests/IpAddressUtilTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiThrottle.Net;
+using Xunit;
+
+namespace WebApiThrottle.Tests
+{
+    public class IpAddressUtilTests
+    {
+        [Fact]
+        public void IsPrivateIpAddress_PrivateAddress_ReturnsTrue()
+        {
+            bool result = IpAddressUtil.IsPrivateIpAddress("10.0.0.1");
+
+            Assert.Equal(true, result);
+        }
+
+        [Fact]
+        public void IsPrivateIpAddress_PublicAddress_ReturnsFalse()
+        {
+            bool result = IpAddressUtil.IsPrivateIpAddress("8.8.8.8");
+
+            Assert.Equal(false, result);
+        }
+
+        [Fact]
+        public void IsPrivateIpAddress_PrivateAddressWithPort_ReturnsTrue()
+        {
+            bool result = IpAddressUtil.IsPrivateIpAddress("10.0.0.1:5555");
+
+            Assert.Equal(true, result);
+        }
+    }
+}
